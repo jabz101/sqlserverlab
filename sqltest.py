0955d121f9e6b7bfe61a0e3865f9a48a35e3a131
Jabed Miah <jabed.miah@uk.clara.net>
2021-01-19 12:10:27 +0000

    2nd commit

--- sqltest.py
+++ sqltest.py
@@ -1,3 +1,4 @@
+import pandas as pd
 import pyodbc
 conn = pyodbc.connect('Driver={SQL Server};'
                       'Server=TESTSQLCLUSTER;'
@@ -10,11 +11,11 @@ cursor = conn.cursor()
 #for row in cursor:
 #    print(row)
 
-cursor.execute('''
-                INSERT INTO AdventureWorks2016.HumanResources.Department (Name, GroupName, ModifiedDate)
-                VALUES ('Ninja' ,'Clan', '2021-01-15 00:00:00:000')
-                ''')
-conn.commit()
+#cursor.execute('''
+#                INSERT INTO AdventureWorks2016.HumanResources.Department (Name, GroupName, ModifiedDate)
+#                VALUES ('Ninja' ,'Clan', '2021-01-15 00:00:00:000')
+#                ''')
+#conn.commit()
 
 #cursor.execute('''
 #                 DELETE FROM AdventureWorks2016.HumanResources.Department
@@ -22,8 +23,12 @@ conn.commit()
 #                 ''')
 #conn.commit()
 
-cursor.execute('SELECT * FROM AdventureWorks2016.HumanResources.Department')
-for row in cursor:
-    print(row)
+sql_query = pd.read_sql_query('SELECT * FROM AdventureWorks2016.HumanResources.Department' , conn)
+print(sql_query)
+print(type(sql_query))
+
+#cursor.execute('SELECT * FROM AdventureWorks2016.HumanResources.Department')
+#for row in cursor:
+#    print(row)
 
 conn.close()
